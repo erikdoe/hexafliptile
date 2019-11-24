c5be0db427985ba80d5c066b477452492564eae2
Erik Doernenburg <erik@doernenburg.com>
2019-11-24 16:41:10 +0100

    Initial Commit

---
+++ MaderaTriangles/MaderaTrianglesView.h
@@ -0,0 +1,13 @@
+//
+//  MaderaTrianglesView.h
+//  MaderaTriangles
+//
+//  Created by Erik Doernenburg on 24/11/2019.
+//Copyright © 2019 Mulle Kybernetik. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MaderaTrianglesView : ScreenSaverView
+
+@end
